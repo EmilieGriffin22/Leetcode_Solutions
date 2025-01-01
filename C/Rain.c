0ab95089a00b1f186de0afca349f250a792eb8c3
EmilieGriffin22 <lelag040804@gmail.com>
2025-01-01 11:58:25 -0500

    Solution to 42. Trapping Rain Water added,

---
+++ C/Rain.c
@@ -0,0 +1,27 @@
+//Original correct solution (optimal)
+int trap(int* height, int heightSize) {
+    int water = 0; 
+    int left = 0; 
+    int right = heightSize; 
+    int leftMax = height[left]; 
+    int rightMax = height[right]; 
+
+    while (left < right) {
+        if (leftMax < rightMax) {
+            if (height[left] > leftMax){
+                leftMax = height[left]
+            }
+            left++; 
+            water += leftMax - height[left]; 
+        } else {
+            if (height[right] > rightMax) {
+                rightMax = height[right]; 
+            }
+            right++; 
+            water += rightMax - height[right]; 
+        }
+    }
+
+    return water; 
+    
+}
